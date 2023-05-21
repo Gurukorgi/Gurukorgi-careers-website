53d2cc972454e8f4648c9460edd5fef90a8fe177
Gurukorgi <miradoxb@gmail.com>
2023-05-21 12:27:38 +0000

    added bootstrap, dynamic data and api route

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS=[
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Abuja, Nigeria',
+    'salary': 'NGN. 500,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Lagos, Nigeria',
+    'salary': 'NGN. 900,000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote'
+  },
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'Cotonou, Benin',
+    'salary': 'CFA. 1,000,000,'
+  }
+]
 
 @app.route('/')
 def hello_gurukorgi():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS,company_name='Cybercube')
+
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == '__main__':

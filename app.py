4b449e0c2949c76bd752227e80c953db3347b6cd
Gurukorgi <miradoxb@gmail.com>
2023-08-01 00:17:27 +0000

    added dynamic pages for each job

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
   
@@ -13,6 +13,17 @@ def hello_gurukorgi():
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
+
+# @app.route("/jobs/<id>")
+# def show_job(id):
+#   job = load_job_from_db(id)
+#   return jsonify(job)
+@app.route('/job/<int:id>')
+def show_job(id):
+    job = load_job_from_db(id)
+    if job is None:
+        return ' JOB NOT FOUND !', 404
+    return render_template('jobpage.html',job=job)
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -14,5 +14,44 @@ def load_jobs_from_db():
         jobs.append(dict(row._asdict()))
     return jobs
 
+# def load_job_from_db(id):
+#   with engine.connect() as conn:
+#     result = conn.execute(
+#       text('SELECT * FROM jobs WHERE id = :val'),val=id
+#     )
+#     rows = result.all()
+#     if len(rows) == 0:
+#       return None
+#     else:
+#       return dict(rows[0])
+
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(
+            text('SELECT * FROM jobs WHERE id = :id'),
+            {"id": id},
+        )
+        row = result.fetchone()
+        if row is None:
+            return None
+        else:
+            job = dict(zip(result.keys(), row))
+            return job
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
